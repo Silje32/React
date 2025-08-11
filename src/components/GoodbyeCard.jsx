001af8ecc3623769f1008a93c2eb1d83de1060a1
Silje32 <msh070425@gmail.com>
2025-08-11 11:51:53 +0200

    Updated my styling

--- src/components/GoodbyeCard.jsx
+++ src/components/GoodbyeCard.jsx
@@ -1,6 +1,6 @@
 export default function GoodbyeCard() {
   return (
-    <div style={{ border: "solid white!", padding: 10 }}>
+    <div style={{ border: "2px solid purple", padding: 5 }}>
       <h3>Goodbye</h3>
     </div>
   );
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,6 +1,6 @@
 export default function Welcome() {
   return (
-    <div style={{ border: "solid white" }}>
+    <div style={{ border: "solid darkblue", padding: 30 }}>
       <h3>Hei fra WelcomeComponent</h3>
       <input type="text" />
       <button>Send</button>
